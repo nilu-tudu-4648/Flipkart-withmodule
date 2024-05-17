5e1c9be775e166cfd109c70d98586b7d3a67f9c6
tudunilesh3@gmail.com <nilesh.tudu@nirmitee.io>
2024-05-17 17:36:35 +0530

    module working

--- a.js
+++ a.js
@@ -1,11 +1,12 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React, { useEffect } from 'react'
-
-import RaptorX from 'react-native-nilesh-module'
+import RaptorX from 'react-native-awesome-module'
 const apiKey = '9a60f01e9b7d2d5d37a1b134241311fd7dfdbc38';
 export const simpleSDK = new RaptorX(apiKey);
 const App = () => {
-  async function getgpu(params) {
+
+  // "react-native-awesome-module": "link:./modules/awesome-module",
+ async function getgpu(params) {
     try {
       const d = await simpleSDK.initDeviceData()
       const loc = await simpleSDK.getNetwork()
@@ -16,6 +17,9 @@ const App = () => {
     }
   }
   useEffect(() => {
+    // Call navigation capture method inside useEffect
+   const data = simpleSDK.initDeviceData();
+   console.log(data)
    getgpu()
   }, []);
   return (
